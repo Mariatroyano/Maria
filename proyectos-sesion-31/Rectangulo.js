711488b315d6d13f0e533b6808e9115f2a28a149
mariatroyano <maria2020troyano@gmail.com>
2024-05-09 15:15:05 -0500

    sesion-41

--- proyectos-sesion-31/Rectangulo.js
+++ proyectos-sesion-31/Rectangulo.js
@@ -1,9 +1,12 @@
 
-function calcularAreaRectangulo(base, altura) {
+
+   
+   
+   function calcularAreaRectangulo(base, altura) {
  
 
     let Area = base * altura
-    let Perimetro = (base + altura)* 2;
+    c
         return {Area, Perimetro};
    }
    let base = parseFloat(prompt("ingrese el area del Rectangulo: "))
@@ -12,8 +15,5 @@ function calcularAreaRectangulo(base, altura) {
    let resultado = calcularAreaRectangulo(base,altura);
    console.log("el area del rectangulo es:" , resultado.Area );
    console.log("perimetro del rectangulo es :", resultado.Perimetro);
-   
-   
-   
    
    
