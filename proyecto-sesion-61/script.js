e5e8562a1469fe01388b3e931fc78928608356b0
mariatroyano <maria2020troyano@gmail.com>
2024-06-05 15:24:58 -0500

    correccion del trabajo sesion 60 con el profe rafael Neflix

--- proyecto-sesion-61/script.js
+++ proyecto-sesion-61/script.js
@@ -2,17 +2,14 @@ const input = document.getElementById("input");
 const button = document.getElementById("button");
 const ul = document.getElementById("ul");
 
-
 button.addEventListener('click', (e) => {//el  even me indica que registre que me agrgue el evento la tarea cn un click
-    e.preventDefault()//lo indique para q cuando mle diera agregar no me cargara la pagina
+    e.preventDefault()//lo indique para q cuando le diera agregar no me cargara la pagina
 //la e puede ser caalquier otro nombre le puse e de evento
-
 
     const text = input.value;//para que me traiga el valor del input y me lo acoja hay
     const li = document.createElement('li');//agrego una constante llamada li que es de mi listadonde cada que agregue seme va estar agregando en lista
     const p = document.createElement(`p`);//y pz me va agregar en forma de parrafito
-    p.textContent = text;
-
+    p.textContent = text;// Asigna el valor del input (almacenado en la variable 'text') como el contenido de texto del párrafo ('p') creado anteriormente.
 
     li.appendChild(p);//para que me p me lo agregue a li que me pase el elemnto
     //para que cada que agregue una tarea me la agregue a un tarea nueva
@@ -27,7 +24,7 @@ function EliminarTarea() {//Para que me de la forma de eliminar una tarea
     EliminarTarea.textContent = "Eliminar";//para que seme agregue el button en cada tarea que agregue
     EliminarTarea.addEventListener('click', (e) => {//que a la hora de dar click me elimine
         const item = e.target.parentElement;//el target se refiere al boton de eliminar paq me acceda al en padre
-        ul.removeChild(item);
+        ul.removeChild(item);///ested es pa q me quite un boton en especifico
 
     })
     return EliminarTarea;//paq me retorne mi ultima funcion
