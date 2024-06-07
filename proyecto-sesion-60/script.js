c4af19a80e5613e6b5ce5162855efc65473e02d5
mariatroyano <maria2020troyano@gmail.com>
2024-06-07 10:34:43 -0500

    agrego de neflix

--- proyecto-sesion-60/script.js
+++ proyecto-sesion-60/script.js
@@ -1,8 +1,14 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const menuIcon = document.getElementById('menu-icon');
-//     const navLinks = document.getElementById('nav-links');
 
-//     menuIcon.addEventListener('click', () => {
-//         navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
-//     });
-// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuIcon.addEventListener('click', () => {
+        navLinks.classList.add('active');
+    })
+
+    nav-links.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    })
+    });
