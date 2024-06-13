712afa71d371e10ed059ffcf3c5a41cad054c291
mariatroyano <maria2020troyano@gmail.com>
2024-06-13 15:42:05 -0500

    arreglo de nefliz

---
+++ proyecto-sesion-67-01/script.js
@@ -0,0 +1,19 @@
+let formulario = document.getElementById("Registro");
+formulario.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  let nombre = document.getElementById("nombre");
+  let apellido = document.getElementById("Apellido");
+  let email = document.getElementById("Email");
+  fetch("https://jsonplaceholder.typicode.com/users&quot", {
+    method: "post",
+    body: JSON.stringify({
+      nombre: nombre,
+      apellido: apellido,
+      email: email,
+    }),
+    headers:{
+      "Content-Type" : "application/json",
+    }
+    })
+  })
+   
