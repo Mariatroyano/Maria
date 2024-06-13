8acbe04522d254ef295caad7a0c2f07cccd75368
mariatroyano <maria2020troyano@gmail.com>
2024-06-13 16:20:47 -0500

    nuevo taller sesion 70

---
+++ proyecto-sesion-70/script.js
@@ -0,0 +1,8 @@
+document.getElementById("commentForm").addEventListener("sudmit",function(event){
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const comment = document.getElementById("comment").value;
+
+    addCommentTolistÑ
+})
