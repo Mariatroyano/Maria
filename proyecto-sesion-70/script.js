930f649810d8e3d9b96dc6bf56039ca81ec39812
Mariatroyano <maria2020troyano@gmail.com>
2024-06-16 16:54:16 -0500

    practica de Neflix

--- proyecto-sesion-70/script.js
+++ proyecto-sesion-70/script.js
@@ -4,5 +4,28 @@ document.getElementById("commentForm").addEventListener("sudmit",function(event)
     const name = document.getElementById("name").value;
     const comment = document.getElementById("comment").value;
 
-    addCommentTolistÑ
+    addCommentTolist(data);
+    document.getElementById("commentForm").reset();
 })
+function addCommentTolist(comment){
+    const commentsList = document.getElementById("commentsList");
+    const commentDiv = document.createElement("div");
+    commentDiv.classList.add("commet");
+
+    const commentAutor = document.createElement("h3");
+    commentAutor.textContent = comment.name;
+
+    const commentBody = document.createElement("p");
+    commentBody.textContent = comment.body;
+
+    commentDiv.appendChild(commentAutor);
+    commentDiv.appendChild(commenBody);
+    commentList.appendChild(commentDiv);
+}
+
+fetch("https://jsonplaceholder.typicode.com/comments . ")
+.then(response => response.json())
+.then(data => {
+    data.forEach(comment => addCommentTolist(comment));
+})
+.catch(error => console.error("Error",error));
