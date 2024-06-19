154b71bf7aaee4320f505293c9e56831dfeb68a1
mariatroyano <maria2020troyano@gmail.com>
2024-06-19 14:59:04 -0500

    nueva sesion 73 terminado con el local storage

--- proyecto-sesion-73/script.js
+++ proyecto-sesion-73/script.js
@@ -2,10 +2,32 @@ const input = document.getElementById("input");
 const button = document.getElementById("button");
 const ul = document.getElementById("ul");
 let arrayTareas = []/*donde creo mi variable para el local storage*/
+const arrayLocalstorage = window.localStorage.getItem("listaTareas")/*creo una constante para q me agregue*/
+console.log(arrayLocalstorage)
 
+let li = ""
+
+if (arrayLocalstorage.length != 0) {
+
+    arrayTareas.push(arrayLocalstorage.split(","));
+    console.log(arrayTareas[0].length)
+    for (i = 0; i < arrayTareas[0].length; i++) { 
+        li = document.createElement('li');
+        li.innerHTML = `${arrayTareas[0][i]}`;/*aqui lo agregamos como forma de array */
+        ul.appendChild(li);
+        const CompletarTarea = document.createElement('button');/*aqui es donde me hace para que a la hora de darle cliclk me cambie como agregado*/
+        CompletarTarea.textContent = "X";
+        CompletarTarea.style.backgroundColor = "#ff0000"
+        li.appendChild(CompletarTarea);
+        li.appendChild(EliminarTarea());
+        CompletarTarea.addEventListener("click", function () {
+            CompletarTarea.style.backgroundColor = "#00ff00"
+            CompletarTarea.textContent = "✔";
+        })
+    }
+}
 button.addEventListener('click', (e) => {
     e.preventDefault()
-
     const text = input.value;
 
     if (text != "") {
@@ -28,10 +50,7 @@ button.addEventListener('click', (e) => {
             CompletarTarea.style.backgroundColor = "#00ff00"
             CompletarTarea.textContent = "✔";
         })
-        window.localStorage.setItem("listaTareas",arrayTareas)/*donde agrego mi local storage*/
-
-        const arrayLocalstorage = window.localStorage.getItem("listaTareas",arrayTareas)/*creo una constante para q me agregue*/
-        console.log(arrayLocalstorage)
+        window.localStorage.setItem("listaTareas", arrayTareas)/*donde agrego mi local storage*/
 
         input.value = "";
     }
@@ -47,3 +66,4 @@ function EliminarTarea() {
     return EliminarTarea;
 
 }
+
---
+++ proyecto-sesion-74/script.js
@@ -0,0 +1,4 @@
+const A =["manzana","banana","piña","kiwi"]
+let B = A.set((item) => item.includes("a"))
+
+console.log(B)
