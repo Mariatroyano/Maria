a450ac4d6f9fea9956aeb72195f8d8264cffd322
mariatroyano <maria2020troyano@gmail.com>
2024-07-12 10:39:49 -0500

    nueva sesion de eventos

---
+++ proyecto-sesion-78-React event/vite-project/src/assets/components/Profiles.jsx
@@ -0,0 +1,22 @@
+import './Profiles.css'
+
+
+function Profiles  ({ Nombre, Imagen, Telefono, Edad, Direccion })  {
+  return (
+      <div className='card-person'>
+          <img
+          className='card-imagen'
+              src={Imagen}
+              alt={Nombre}
+          />
+          
+          <div className='card-content'>
+              <h3>{Nombre}</h3>
+              <p>Telefono:{Telefono}</p>
+              <p>Edad:{Edad}</p>
+              <p> Direccion:{ Direccion}</p>
+          </div>
+      </div>
+  )
+}
+export default Profiles
