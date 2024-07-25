2dc72d97d9ad40cef8171b54cd12dbf6f6e625be
mariatroyano <maria2020troyano@gmail.com>
2024-07-25 12:00:46 -0500

    arreglo

---
+++ express/index.js
@@ -0,0 +1,24 @@
+// const express = require('express')
+// const 
+// const app = express()
+// const port = 3000
+
+// app.get('/InfoMemoria', (req, res) => {
+//   res.send('totalmem!')
+// })
+// app.get('/InfoGeneral', (req, res) => {
+//   res.os.PlatForm()
+// })
+// app.get('/Arquitectura', (req, res) => {
+//   res.os.Arch()
+// })
+// app.get('/nombreSistema', (req, res) => {
+//   res.os.type()
+// })
+// app.get('/TiempoSistema', (req, res) => {
+//   res.os.Uptime()
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
