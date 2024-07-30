e56a277cdf89f6164d7ba7f159d0ef67a2ba3f0c
Mariatroyano <maria2020troyano@gmail.com>
2024-07-30 15:23:35 -0500

    arreglo

--- express-postman/index.js
+++ express-postman/index.js
@@ -1,20 +1,31 @@
 const express = require("express");
+const app = express();
+ const port = 3000;
 
 const products = [
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 2, name: 'Product 2', price: 200 },
-    { id: 3, name: 'Product 3', price: 300 }
+    { id: 1, name: 'iphone 15', price: 5000000,date:'Agosto' },
+    { id: 2, name: 'Samsung', price:2000000,date:'Noviembre'  },
+    { id: 3, name: 'Xiaomi', price: 500000,date:'Diciembre'  },
+    { id: 4, name: 'appo', price: 700000,date:'Enero'  },
+    { id: 5, name: 'Motorola', price: 500000,date:'Agosto'  },
+    { id: 6, name: 'Huawey', price: 500000,date:'Agosto' },
   ];
 const categoria = [
-    { id: 1, name: 'Amazon',amount:2 },
-    { id: 2, name: 'H&Y',amount:5  },
-    { id: 3, name: 'IKEY',amount:3 }
+    { id: 1, name: 'Galaxy A',amount:2 },
+    { id: 2, name: 'Galaxy M',amount:5  },
+    { id: 3, name: 'Galaxy s23',amount:3 },
+    { id: 4, name: 'Samsung Galaxy Z Flip5',amount:3 },
+    { id: 5, name: 'Samsung Galaxy A03 Core',amount:3 },
+    { id: 6, name: 'Samsung Galaxy A13',amount:3 },
   ];
 
 const cliente = [
-    { id: 1, name: 'Karina River' },
-    { id: 2, name: 'Sebastian Quinayas'},
-    { id: 3, name: 'Nicol Ramos'}
+    { id: 1, name: 'Karina Rivera',phone:3245678997,correo:'karina23@mail.com',Dirreccion:'popayán'},
+    { id: 2, name: 'Sebastian Quinayas',phone:323245678,correo:'sebastian45@mail.com',Dirreccion:'cali'},
+    { id: 3, name: 'Nicol Camayo',phone:3235678978,correo:'tapasco2345@mail.com',Dirreccion:'popayán'},
+    { id: 4, name: 'Andres Urrego',phone:3245678997,correo:'AndresUrrego23@mail.com',Dirreccion:'popayán'},
+    { id: 5, name: 'Cristian Yalanda',phone:3104567896,correo:'cris2323@mail.com',Dirreccion:'popayán'},
+    { id: 6, name: 'Maria Troyano',phone:3123456786,correo:'Maria2023@mail.com',Dirreccion:'popayán'}
   ];
   
   // Ruta para obtener todos los productos
@@ -43,7 +54,6 @@ const cliente = [
       res.status(404).json({ message: 'Product not found' });
     }
   });
-  app.listen(port,() => {
-    console.log(`servidor http://localhost:${port}`);
-    
-})
+  app.listen(port, () => {
+    console.log(`Puerto Escuchado ${port}`);
+});
