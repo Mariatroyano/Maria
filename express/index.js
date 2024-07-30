da3d38b80ed7cf74f9680a2917bd399c586f9a82
Mariatroyano <maria2020troyano@gmail.com>
2024-07-30 08:43:14 -0500

    arreglo express

--- express/index.js
+++ express/index.js
@@ -1,52 +1,45 @@
 
-
 const express = require('express');
 const os = require('os');
 const app = express();
 const port = 3000;
 
 app.get('/InfoMemoria', (req, res) => {
+    const memoriatotal = os.totalmem();
     res.send(`Total memory: ${memoriatotal}`);
-  const memoriatotal = os.totalmem();
-  console.log(memoriatotal);
-
+    console.log(memoriatotal);
 });
 
 app.get('/InfoGeneral', (req, res) => {
-  res.send(`Platform: ${plataforma}`);
-  const plataforma = os.platform();
-  console.log(plataforma); // win32
- 
+    const plataforma = os.platform();
+    res.send(`Platform: ${plataforma}`);
+    console.log(plataforma); // win32
 });
 
 app.get('/Arquitectura', (req, res) => {
-  res.send(`CPU Architecture: ${cpuArchitecture}`);//temprey
-  const cpuArchitecture = os.arch();
-  console.log(cpuArchitecture); // x64
-
+    const cpuArchitecture = os.arch();
+    res.send(`CPU Architecture: ${cpuArchitecture}`);
+    console.log(cpuArchitecture); // x64
 });
 
 app.get('/nombreSistema', (req, res) => {
-  res.send(`OS Type: ${nombreOs}`);
-  const nombreOs = os.type();
-  console.log(nombreOs); // Windows_NT
-
+    const nombreOs = os.type();
+    res.send(`OS Type: ${nombreOs}`);
+    console.log(nombreOs); // Windows_NT
 });
 
 app.get('/TiempoSistema', (req, res) => {
-  res.send(`Uptime: ${uptimeOS} seconds`);
-  const uptimeOS = os.uptime();//rangos flotantes,cademas de caracteres o estrin 
-  console.log(uptimeOS); // 95156
-
+    const uptimeOS = os.uptime();
+    res.send(`Uptime: ${uptimeOS} seconds`);
+    console.log(uptimeOS); // 95156
 });
 
 app.get('/Procesador', (req, res) => {
-  res.send(cpusCoreLogic);
-  const cpusCoreLogic = os.cpus();
-  console.log(cpusCoreLogic);
-  res.send(cpusCoreLogic);
+    const cpusCoreLogic = os.cpus();
+    res.json(cpusCoreLogic);
+    console.log(cpusCoreLogic);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Puerto Escuchado ${port}`);
 });
