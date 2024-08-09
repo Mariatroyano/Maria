1899ccd2f54430de6c1d15e809de07d3c1041100
Mariatroyano <maria2020troyano@gmail.com>
2024-08-09 11:32:30 -0500

    express proyecto-carrito

--- expres-posgres/index.js
+++ expres-posgres/index.js
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const sequelize = new Sequelize("crud-express", "troyano", "123456789", {
+const sequelize = new Sequelize("user", "maria", "12345678", {
   host: "localhost",
   dialect: "postgres",
   logging: false,
@@ -15,18 +15,19 @@ const sequelize = new Sequelize("crud-express", "troyano", "123456789", {
 
 // Definir el modelo de datos para 'User'
 const User = sequelize.define(
-  "User",
+  "user",
   {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    email: { 
-      type: DataTypes.STRING, 
-      allowNull: false, 
-      unique: true },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
   },
-  { 
+  {
     timestamps: false,
     tableName: "users", // Nombre personalizado para la tabla
   }
@@ -59,39 +60,166 @@ app.post("/users", async (req, res) => {
   }
 });
 // Actualizar un usuario
-// app.put("/users/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const { name, email } = req.body;
-//   try {
-//     const user = await User.findByPk(id);
-//     if (user) {
-//       user.name = name;
-//       user.email = email;
-//       await user.save();
-//       res.json(user);
-//     } else {
-//       res.status(404).json({ error: "Usuario no encontrado" });
-//     }
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// });
+app.put("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  try {
+    const user = await User.findByPk(id);
+    if (user) {
+      user.name = name;
+      user.email = email;
+      await user.save();
+      res.json(user);
+    } else {
+      res.status(404).json({ error: "Usuario no encontrado" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 // // Eliminar un usuario
-// app.delete("/users/:id", async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     const user = await User.findByPk(id);
-//     if (user) {
-//       await user.destroy();
-//       res.json({ message: "Usuario eliminado" });
-//     } else {
-//       res.status(404).json({ error: "Usuario no encontrado" });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByPk(id);
+    if (user) {
+      await user.destroy();
+      res.json({ message: "Usuario eliminado" });
+    } else {
+      res.status(404).json({ error: "Usuario no encontrado" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+
+//............................................................................................
+
+
+
+
+
+// Iniciar el servidor en el puerto 3000
+app.listen(3000, () => {
+  console.log("Servidor ejecutándose en http://localhost:3000");
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// const sequelize = new Sequelize("user", "postgres", "postgres", {
+//   host: "localhost",
+//   dialect: "postgres",
+//   logging: false,
 // });
+
+// Definir el modelo de datos para 'User'
+const cliente = sequelize.define(
+  "cliente",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    Age: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    Phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: "users", // Nombre personalizado para la tabla
+  }
+);
+
+// Sincronizar el modelo con la base de datos (crear la tabla si no existe)
+sequelize
+  .sync()
+  .then(() => console.log("Conectado a la base de datos y sincronizado."))
+  .catch((err) => console.error("Error al conectar a la base de datos:", err));
+// Rutas CRUD
+// Obtener todos los usuarios
+app.get("/cliente", async (req, res) => {
+  try {
+    const cliente = await cliente.findAll();
+    res.json(cliente);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Crear un nuevo usuario
+app.post("/cliente", async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const newCliente = await cliente.create({ name, lastname,  Age,email ,  Phone, address});
+    res.status(201).json(newCliente);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+// Actualizar un usuario
+app.put("/cliente/:id", async (req, res) => {
+  const { id } = req.params;
+  const {  name, lastname,  Age,email ,  Phone, address} = req.body;
+  try {
+    const cliente = await User.findByPk(id);
+    if (cliente) {
+      cliente.name = name;
+      cliente.lastname = lastname;
+      cliente. Age =  Age;
+      cliente.email = email;
+      cliente.Phone = Phone;
+      cliente. address = address;
+      await cliente.save();
+      res.json(cliente);
+    } else {
+      res.status(404).json({ error: "Usuario no encontrado" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// // Eliminar un usuario
+app.delete("/cliente/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const cliente = await cliente.findByPk(id);
+    if (cliente) {
+      await cliente.destroy();
+      res.json({ message: "Usuario eliminado" });
+    } else {
+      res.status(404).json({ error: "Usuario no encontrado" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 // Iniciar el servidor en el puerto 3000
 app.listen(3000, () => {
   console.log("Servidor ejecutándose en http://localhost:3000");
